6c00d585371c1a8a0c0f5431998d98cfed9d20db
Sanya <4eh91@mail.ru>
2020-10-31 14:57:09 +0200

    added desKtop

--- app/js/main.js
+++ app/js/main.js
@@ -4,28 +4,16 @@ $(function () {
         // infinite: true,
         speed: 2500,
         dots: true,
-        // slidesToShow: 1,
-        // centerMode: true,
-        // centerPadding: "120px",
-        // variableWidth: true,
-        // autoplay: true,
-        // asNavFor: ".gallery__title",
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-        // responsive: [
-        //     {
-        //       breakpoint: 700,
-        //       settings: {
-        //         centerPadding: "0px",
-        //         centerMode: true,
-        //         // cssEase: 'linear',
-        //         fade: true,
-        //         slidesToShow: 1,
-        //         variableWidth: false,
-        //       }
-        //     },
-        // ]
     });
+
+    $('.review__slider').slick({
+        speed: 1500,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    })
 
 
 });
