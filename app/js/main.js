7939fdeee2624a01ee76f709770532238f189431
Sanya <4eh91@mail.ru>
2020-11-02 13:44:17 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,15 @@ $(function () {
         dots: true,
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 701,
+                settings: {
+                prevArrow: false,
+                nextArrow: false
+                }
+            }
+        ]
     });
 
     $('.review__slider').slick({
@@ -13,7 +22,34 @@ $(function () {
         dots: true,
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 701,
+                settings: {
+                prevArrow: false,
+                nextArrow: false
+                }
+            }
+        ]
     })
 
-
 });
+
+const topMobileBtn = document.querySelector(".top__wrapper-mobile-btn");
+const headerWrapper = document.querySelector(".header__wrapper");
+const topSearch = document.querySelector(".top__wrapper-mobile-search");
+const topInner = document.querySelector(".top__inner");
+
+
+const openMenu = () => {
+    headerWrapper.classList.toggle("open");
+}
+
+topMobileBtn.addEventListener("click", openMenu);
+
+
+const openSearch = () => {
+    topInner.classList.toggle("open");
+}
+
+topSearch.addEventListener("click", openSearch);
